ab1c5fa66acd953765b59d48bd8ce042a2eb929c
Tran Huyen <tranhuyennhc2004@gmail.com>
2025-03-17 19:26:36 +0700

    css lai image cua post

--- src/containers/AboutUs.jsx
+++ src/containers/AboutUs.jsx
@@ -1,8 +1,24 @@
+"use client"
+import Image from "next/image";
+
 const AboustUs = (AboustUs) =>{
-    console.log(AboustUs);
+    console.log(AboustUs)
     return(
         <>
-        About us
+        <div style={{ textAlign: "center" }}>
+                    <img
+                        src="/images/Topbanner.jpg" 
+                        
+                        alt="Banner Tin tức"
+                        style={{ width: "100%",
+                            maxHeight: "500px",
+                            objectFit: "cover",
+                            overlay: "rgba(0,0,0,0.483)",
+                            backgroundColor: "rgb(255,255,255)"
+
+                            }}
+                    />
+                </div>
         </>
     )
 }
