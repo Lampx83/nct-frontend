856466faa5cd88c33cfc46df4120067390542c28
diepanhnguyen2807 <diepanhnguyen2807@gmail.com>
2025-03-26 15:10:44 +0700

    facts carousel

--- src/containers/TrangChinh.jsx
+++ src/containers/TrangChinh.jsx
@@ -5,14 +5,17 @@ import { Intro } from "../components/trangchinh/intro";
 import { Events } from "../components/trangchinh/sukien";
 import { FaqSection } from "../components/trangchinh/cauhoi";
 import VideoBackground from '../components/VideoBackground';
+import Service from "@/components/home/Service";
 import StatsSection from '../components/trangchinh/fact';
 import Khoa from '../components/trangchinh/khoa';
+import CustomCarousel from "@/components/home/CustomCarousel";
 
 export default function TrangChinh() { 
 
   return (
     <>
         <VideoBackground/>
+        <Service/>
         <Intro/>
         <Khoa/>
         <StatsSection/>
