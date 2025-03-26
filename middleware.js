f59bce6800ed615c689cadcf75ef82d36426ba8a
duyviet2101 <whoisduyviet@gmail.com>
2025-03-26 10:10:18 +0700

    self host

--- middleware.js
+++ middleware.js
@@ -4,11 +4,8 @@ export function middleware(request) {
   const url = request.nextUrl.clone();
   const rewrites = [
     { path: "/admin", destination: "http://101.96.66.218:8020" },
-    { path: "/oj", destination: "http://203.113.132.48:8016" },
     { path: "/codelab", destination: "http://101.96.66.217:8015" },
     { path: "/search_api", destination: "http://101.96.66.219:8003" },
-    { path: "/scoreup", destination: "https://scoreup-frontend.vercel.app/" },
-    { path: "/neu-oj", destination: "https://neu-oj-frontend.vercel.app/neu-oj" },
   ];
 
   for (const { path, destination } of rewrites) {
