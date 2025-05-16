76b96be164bd802898f21505cb4a0373b1bd572e
Tran Huyen <tranhuyennhc2004@gmail.com>
2025-05-16 11:17:58 +0700

    tac gia bai dang

--- src/components/tin-tuc/NewsDetails.jsx
+++ src/components/tin-tuc/NewsDetails.jsx
@@ -43,6 +43,7 @@ figure.table {
 
 const NewsDetails = ({ newsData }) => {
   console.log(newsData);
+  console.log(newsData.createdBy.data.attributes.lastname);
   return (
     <div>
       <style>{styles}</style>
@@ -53,7 +54,7 @@ const NewsDetails = ({ newsData }) => {
           height: "500px",
           backgroundSize: "cover",
           backgroundPosition: "center",
-          fontFamily:"Roboto sans-serif !important",
+          fontFamily: "Roboto sans-serif !important",
 
         }}
       >
@@ -63,18 +64,29 @@ const NewsDetails = ({ newsData }) => {
         >
           <div className="container text-center text-white">
             <h2 className="display-7 text-light my-4 ">{newsData?.title || "Tin tức"}</h2>
-            <hr style={{ borderBottom: "2px solid gray", width: "50px", margin: "10px auto" }}/>
+            <hr style={{ borderBottom: "2px solid gray", width: "50px", margin: "10px auto" }} />
             <p className="text-light mt-3">
-              Tác giả Editor NST đăng vào {moment(newsData.eventDate || newsData.createdAt).format("DD/MM/YYYY")}
+              <span className="me-1">
+                <i className="far fa-user-circle" style={{ fontSize: "18px" }}></i>
+                <span className="ms-2">{newsData.createdBy.data.attributes.firstname}</span>
+                <span className="ms-1">{newsData.createdBy.data.attributes.lastname}</span>
+              </span>
+              <span className="ms-1">
+                <i className="far fa-clock" style={{ fontSize: "18px" }}></i>
+                <span className="ms-2">
+                  {moment(newsData.eventDate || newsData.createdAt).format("DD/MM/YYYY")}
+                </span>
+              </span>
             </p>
+
           </div>
         </div>
       </div>
       <div className="container p-3">
         <div dangerouslySetInnerHTML={{ __html: newsData?.content || "Không có nội dung" }}></div>
       </div>
-      </div>
-    
+    </div>
+
 
   );
 };
