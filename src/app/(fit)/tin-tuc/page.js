78f04bba354443446fc6b224c7bd1609dee3277c
Tran Huyen <tranhuyennhc2004@gmail.com>
2025-05-21 23:23:58 +0700

    tat cache

--- src/app/(fit)/tin-tuc/page.js
+++ src/app/(fit)/tin-tuc/page.js
@@ -2,7 +2,10 @@ import Tintuc from "@/components/tin-tuc/Tintuc";
 
 async function getBlogs() {
     const response = await fetch(
-      `https://nct-frontend-liard.vercel.app/admin/api/blogs?populate=*`
+      `https://nct-frontend-liard.vercel.app/admin/api/blogs?populate=*`,
+      {
+        cache: "no-store"
+      }
     );
     const data = await response.json();
 
