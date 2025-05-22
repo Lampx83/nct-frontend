f7d56d1332989fad0a9a8604afe760ef9126cf37
Tran Huyen <tranhuyennhc2004@gmail.com>
2025-05-22 09:54:51 +0700

    sap xep tin tuc theo ngay dang moi nhat

--- src/app/(fit)/tin-tuc/page.js
+++ src/app/(fit)/tin-tuc/page.js
@@ -2,7 +2,7 @@ import Tintuc from "@/components/tin-tuc/Tintuc";
 
 async function getBlogs() {
     const response = await fetch(
-      `https://nct-frontend-liard.vercel.app/admin/api/blogs?populate=*`,
+      `https://nct-frontend-liard.vercel.app/admin/api/blogs?populate=*&sort=createdAt:desc`,
       {
         cache: "no-store"
       }
