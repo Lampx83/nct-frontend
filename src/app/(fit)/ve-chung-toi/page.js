9818ff35c8590a41ce62b51d097d65d7ab5e69d9
Tran Huyen <tranhuyennhc2004@gmail.com>
2025-05-24 15:30:33 +0700

    cache gioi thieu

--- src/app/(fit)/ve-chung-toi/page.js
+++ src/app/(fit)/ve-chung-toi/page.js
@@ -1,7 +1,10 @@
 import AboustUs from "@/containers/AboutUs";
 async function getAboutUs() {
     const response = await fetch(
-      `https://nct.neu.edu.vn/admin/api/about-us-page?populate=*`
+      `https://nct.neu.edu.vn/admin/api/about-us-page?populate=*`,
+      {
+        cache: "no-cache"
+      }
     );
     const data = await response.json();
 
