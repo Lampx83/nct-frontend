f7a0fc7ca156d75cf0325be9cc3d2b4508f18515
Tran Huyen <tranhuyennhc2004@gmail.com>
2025-07-15 06:58:14 +0700

    overview

--- src/app/(fit)/gioi-thieu-chung/page.js
+++ src/app/(fit)/gioi-thieu-chung/page.js
@@ -1,7 +1,20 @@
 import Overview from "@/containers/Overview";
 
-export default function Page() {
+
+async function getOverview() {
+    const res = await fetch ('https://nct.neu.edu.vn/admin/api/introduction?populate=deep,3', {
+        cache: "no-cache"
+    });
+    const data = await res.json();
+
+    return data.data.attributes.content;
+    
+}
+
+
+export default async function Page() {
+    const dataOverview = await getOverview();
     return (
-        <Overview/>
+       <div> <Overview dataOverview = {dataOverview}/></div>
     );
 }
